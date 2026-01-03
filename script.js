1a7f592a99272c47719dfca24a154b669178aa2e
COULDBLAST <hernandezultimofredy@gmail.com>
2026-01-02 19:18:37 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+fetch('productos.json')
+  .then(res => res.json())
+  .then(productos => {
+    const contenedor = document.getElementById('catalogo-tenis');
+
+    productos.forEach((p, i) => {
+      contenedor.innerHTML += `
+        <div class="producto" data-tallas="${p.tallas.join(',')}">
+          <img src="${p.imagen}" alt="${p.nombre}">
+          <div class="info">
+            <h2>${p.nombre}</h2>
+            <p>Q${p.precio}</p>
+            <select onchange="cambiarTalla(this, 'talla${i}')">
+              <option value="">Verifica tallas disponibles</option>
+              ${p.tallas.map(t => `<option value="${t}">${t}</option>`).join("")}
+            </select>
+            <div class="talla-seleccionada" id="talla${i}">Talla: --</div>
+          </div>
+        </div>
+      `;
+    });
+  });
